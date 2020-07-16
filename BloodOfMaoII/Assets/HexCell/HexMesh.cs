5626b428c55c40e5791f98d2a2472f52417cb00c
TheDizzler <tgillon.music@gmail.com>
2020-07-17 01:45:40 +0900

    annnd |*| thunk |*|   3d meshes from noisemap with hex grid

--- BloodOfMaoII/Assets/HexCell/HexMesh.cs
+++ BloodOfMaoII/Assets/HexCell/HexMesh.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using AtomosZ.BoMII.Terrain;
+using AtomosZ.BoMII.Terrain.Generation;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using static AtomosZ.BoMII.Terrain.HexTools;
@@ -8,8 +9,12 @@ public class HexMesh : MonoBehaviour
 {
 	public Tilemap tilemap;
 	public TerrainTile testTile;
+	public int mapTileWidth, mapTileHeight;
+	public NoiseSettings noiseSettings;
+
 	private float hexHeight;
 	private float hexWidth;
+
 
 	public void MakeMesh()
 	{
@@ -24,18 +29,19 @@ public class HexMesh : MonoBehaviour
 		hexWidth = Mathf.Sqrt(diagonalDist * diagonalDist - Mathf.Pow(hexHeight * .5f, 2)) * 4 / 3;
 
 		tilemap.ClearAllTiles();
-		//TerrainTile newTile = Instantiate(testTile);
-		//tilemap.SetTile(Vector3Int.zero, newTile);
-		//tilemap.SetTile(HexTools.GetAdjacentTileOffset(Vector3Int.zero, Cardinality.NE), newTile);
-		//tilemap.SetTile(HexTools.GetAdjacentTileOffset(Vector3Int.zero, Cardinality.SE), newTile);
 
-		List<TerrainTile> tts = new List<TerrainTile>()
+		Vector3Int[] positions = new Vector3Int[mapTileWidth * mapTileHeight];
+		TileBase[] tileArray = new TileBase[positions.Length];
+
+		for (int index = 0; index < positions.Length; index++)
 		{
-			GetTile(Vector3Int.zero),
-			GetTile(HexTools.GetAdjacentTileOffset(Vector3Int.zero, Cardinality.NE)),
-			//GetTile(HexTools.GetAdjacentTileOffset(Vector3Int.zero, Cardinality.SE)),
-		};
-		MeshIt(new List<Vector3Int>() { Vector3Int.zero, HexTools.GetAdjacentTileOffset(Vector3Int.zero, Cardinality.NE), HexTools.GetAdjacentTileOffset(Vector3Int.zero, Cardinality.SE) });
+			positions[index] = new Vector3Int(index % mapTileWidth, index / mapTileHeight, 0);
+			tileArray[index] = testTile;
+		}
+		tilemap.SetTiles(positions, tileArray);
+
+		float[,] noiseMap = Noise.GenerateNoiseMap(4 * mapTileWidth, 3 * mapTileHeight, noiseSettings, Vector2.zero);
+		MeshIt(positions, noiseMap);
 	}
 
 
@@ -44,23 +50,26 @@ public class HexMesh : MonoBehaviour
 		return tilemap.GetTile<TerrainTile>(offsetGridCoords);
 	}
 
-	//private void MeshIt(List<TerrainTile> tiles)
-	private void MeshIt(List<Vector3Int> tiles)
+	//private void MeshIt(List<TerrainTile> tiles, float[,] noiseMap)
+	private void MeshIt(Vector3Int[] tiles, float[,] noiseMap)
 	{
 		Vector3 toTopLeft = -Vector3.right * .25f * hexWidth + Vector3.up * .5f * hexHeight;
 		Vector3 toTopRight = Vector3.right * .25f * hexWidth + Vector3.up * .5f * hexHeight;
 		Vector3 toRight = Vector3.right * .5f * hexWidth;
 
 		Dictionary<Vector3, int> vertexCodex = new Dictionary<Vector3, int>();
-		int[] triangles = new int[tiles.Count * 12];
+		int[] triangles = new int[tiles.Length * 12];
 		int vertexIndex = 0;
 		int triangleIndex = 0;
+		float minX = float.MaxValue;
+		float maxX = float.MinValue;
+		float minY = float.MaxValue;
+		float maxY = float.MinValue;
 
 		//foreach (TerrainTile tile in tiles)
 		foreach (Vector3Int tile in tiles)
 		{
 			Vector3 tileCenter = tilemap.GetCellCenterWorld(tile);
-
 
 			Vector3 topLeftCorner = tileCenter + toTopLeft;
 			Vector3 topRightCorner = tileCenter + toTopRight;
@@ -69,13 +78,10 @@ public class HexMesh : MonoBehaviour
 			Vector3 bottomLeftCorner = tileCenter - toTopRight;
 			Vector3 leftCorner = tileCenter - toRight;
 
-
-			//Vector3[] vertices = new Vector3[]
-			//{
-			//	topLeftCorner, topRightCorner, rightCorner,
-			//	bottomRightCorner, bottomLeftCorner, leftCorner
-			//};
-
+			minX = Mathf.Min(minX, leftCorner.x);
+			maxX = Mathf.Max(maxX, rightCorner.x);
+			minY = Mathf.Min(minY, bottomRightCorner.y);
+			maxY = Mathf.Max(maxY, topRightCorner.y);
 
 			if (!vertexCodex.TryGetValue(topLeftCorner, out int tlIndex))
 			{
@@ -89,13 +95,11 @@ public class HexMesh : MonoBehaviour
 				vertexCodex[topRightCorner] = trIndex;
 			}
 
-
 			if (!vertexCodex.TryGetValue(rightCorner, out int rIndex))
 			{
 				rIndex = vertexIndex++;
 				vertexCodex[rightCorner] = rIndex;
 			}
-
 
 			if (!vertexCodex.TryGetValue(bottomRightCorner, out int brIndex))
 			{
@@ -103,13 +107,11 @@ public class HexMesh : MonoBehaviour
 				vertexCodex[bottomRightCorner] = brIndex;
 			}
 
-
 			if (!vertexCodex.TryGetValue(bottomLeftCorner, out int blIndex))
 			{
 				blIndex = vertexIndex++;
 				vertexCodex[bottomLeftCorner] = blIndex;
 			}
-
 
 			if (!vertexCodex.TryGetValue(leftCorner, out int lIndex))
 			{
@@ -133,9 +135,19 @@ public class HexMesh : MonoBehaviour
 		}
 
 		Vector3[] vertices = new Vector3[vertexCodex.Keys.Count];
+
+		int mapX = noiseMap.GetLength(0);
+		int mapY = noiseMap.GetLength(1);
+
 		foreach (var kvp in vertexCodex)
 		{
-			vertices[kvp.Value] = kvp.Key;
+			Vector3 pos = kvp.Key;
+			float percentX = Mathf.InverseLerp(minX, maxX, pos.x);
+			float percentY = Mathf.InverseLerp(minY, maxY, pos.y);
+			int x = Mathf.RoundToInt(percentX * (mapX - 1));
+			int y = Mathf.RoundToInt(percentY * (mapY - 1));
+
+			vertices[kvp.Value] = pos + new Vector3(0, 0, noiseMap[x, y] * 10);
 		}
 
 		Mesh mesh = new Mesh();
